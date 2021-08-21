00818f72deb5222428484d4a21e7f7b72059e259
Zakir Magdum <zakirmagdum2000@yahoo.com>
2021-08-20 20:52:25 -0400

    Added scholors to episode

--- index.js
+++ index.js
@@ -17,17 +17,22 @@ firebase.initializeApp({
   
 var db = firebase.firestore();
 
+let speakers = {}
+JSON.parse(fs.readFileSync('scholors.json')).forEach(spkr => {
+    db.collection("scholors").doc(spkr.name).set(spkr);
+    speakers[spkr.name] = spkr
+});
 let rawdata = fs.readFileSync('categories.json');
 let series = JSON.parse(rawdata);
-
 series.forEach(element => {
     element.updated = Date.now()
-    db.collection("catagories").doc(element.title).set(element);
-    console.log('added/updated category' + element.title);
+    // db.collection("catagories").doc(element.title).set(element);
+    // console.log('added/updated category' + element.title);
 
-    if (element.feedUrl && element.artwork == '40hadith') {
+    if (element.feedUrl /*&& element.artwork == '40hadith'*/) {
         rssParser.parseURL(element.feedUrl).then(feed => {
-            console.log(feed.title);
+            let categories = {}
+            console.log(feed.title, feed.items.length);
             feed.items.forEach(item => {
                 var episode = {
                     title: item.title,
@@ -37,23 +42,50 @@ series.forEach(element => {
                     content: item.content,
                     contentSnippet: item.contentSnippet,
                     isoDate: Date.parse(item.isoDate),
-                    subtitle: item.itunes.subtitle, 
+                    subtitle: item.itunes && item.itunes.subtitle ? item.itunes.subtitle : 'bad-subtitle', 
                     duration: parseDuration(item.itunes.duration),
-                    image: item.itunes.image,
-                    keywords: item.itunes.keywords,
+                    image: item.itunes && item.itunes.image ? item.itunes.image : 'bad-image',
+                    keywords: item.itunes && item.itunes.keywords ? item.itunes.keywords : 'bad-keywords',
                     streamUrl: item.enclosure ? item.enclosure.url : 'nourl',
                     category: element.title,
+                    speaker: getSpeaker(item.categories, speakers),
                     updated: Date.now(), 
                 }
-                id = uuidv5(item.enclosure.url, MY_NAMESPACE);
+                id = uuidv5(item.enclosure ? item.enclosure.url : 'bad', MY_NAMESPACE);
+                if (episode.speaker == "unknown") {
+                    getSpeaker(item.categories, speakers)
+                    console.log("no speaker", item, episode)
+                }
                 db.collection("episodes").doc(id).set(episode);
             });
+            console.log(categories)
         });
     }
 
 }); 
 
+const getSpeaker = function(categories, speakers) {
+    if (categories) {
+        for (c in categories) {
+            if (speakers.hasOwnProperty(categories[c])) {
+                return categories[c];
+            }
+        }
+        for (const [key, spkr] of Object.entries(speakers)) { 
+             for (const alias of spkr.aliases) {
+                if (categories.includes(alias)) {
+                    return key;
+                }
+            }
+        }
+    }
+    return "unknown";
+}
+
 const parseDuration = function(str) {
+    if (str === undefined) {
+        return 3600;
+    }
     const tokens = str.split(':');
     duration = 0;
     if (tokens.length > 2) {
@@ -73,9 +105,9 @@ const parseDuration = function(str) {
 
 const shortTitle = function(title, category) {
     let short = title.replace(category, '').trim()
-    if (short.charAt(0) == '-' || short.charAt(0) == ':') {
+    if (short.charAt(0) == "–" || short.charAt(0) == ":") {
         short = short.substring(1).trim()
-    }
+     }
     return short;
 }
 // db.collection("users").add({
